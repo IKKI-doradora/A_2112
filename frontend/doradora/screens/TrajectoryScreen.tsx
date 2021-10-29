bd4d2c65d4627608619fdbdfccec31d18d7e4618
urare <tomoki.urata.r4@dc.tohoku.ac.jp>
2021-10-30 03:48:34 +0900

    ログ出力を削除

--- frontend/doradora/screens/TrajectoryScreen.tsx
+++ frontend/doradora/screens/TrajectoryScreen.tsx
@@ -19,41 +19,25 @@ export default function TrajectoryScreen() {
     
         if (!result.cancelled) {
             _getTrajectory(result.uri);
-            console.log(result.uri);
         }
     }
 
     const _getTrajectory = async (uri: string) => {
         setSourceUri(uri);
         const video = await FileSystem.readAsStringAsync(uri, { encoding: 'base64' });
-        console.log(video.length);
-
-        // キャッシュへの書き込みテスト
-        // const newUri = FileSystem.cacheDirectory + "trajectory.mov";
-        // await FileSystem.writeAsStringAsync(newUri, video, { encoding: 'base64' });
-        // setTrajectoryUri(newUri);
        
         fetch("http://192.168.43.129:5000/trajectory", {
             method: "POST",
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({base64: video}),
         })
-        .then(res => {console.log(res); return res.json()})
-        .then(async data => {
-            const newUri = FileSystem.cacheDirectory + "trajectory.mov";
-            await FileSystem.writeAsStringAsync(newUri, data.base64mp4, { encoding: "base64" });
-            setTrajectoryUri(newUri);
-        });
-
-        // 通信テスト
-        // fetch("http://proc.memotube.xyz/video", {
-        //     method: "POST",
-        //     headers: {'Content-Type': 'application/json'},
-        //     body: JSON.stringify({base64: video}),
-        // })
-        //     .then(res => {return res.text()})
-        //     .then(data => console.log(data));
-        
+            .then(res => res.json())
+            .then(async data => {
+                const newUri = FileSystem.cacheDirectory + "trajectory.mov";
+                await FileSystem.writeAsStringAsync(newUri, data.base64mp4, { encoding: "base64" });
+                setTrajectoryUri(newUri);
+            });
+    
     }
     
 
@@ -62,7 +46,7 @@ export default function TrajectoryScreen() {
         if (Platform.OS !== 'web') {
             const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
             if (status !== 'granted') {
-            alert('Sorry, we need camera roll permissions to make this work!');
+                alert('Sorry, we need camera roll permissions to make this work!');
             }
         }
         })();
@@ -72,7 +56,6 @@ export default function TrajectoryScreen() {
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <View style={{flex: 1, alignItems: 'center', justifyContent: 'center' }}>
                 <TouchableOpacity onPress={pickVideo} style={styles.button}><Text>Pick an video from camera roll</Text></TouchableOpacity>
-                {/* <TouchableOpacity onPress={() => {setTrajectoryUri(uri => uri)}} style={styles.button}/> */}
             </View>
             <View style={{flex: 5, alignItems: 'center', justifyContent: 'center' }}>
                 {trajectoryUri ? (
