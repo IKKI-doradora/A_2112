591101051b138a3dbbcf58bec1963bf524080dab
Kumagai <koshiro.kumagai.r7@dc.tohoku.ac.jp>
2021-11-08 15:13:41 +0900

    Analytics Chart のリファクタリング

--- frontend/doradora/screens/AnalyticsScreen.tsx
+++ frontend/doradora/screens/AnalyticsScreen.tsx
@@ -65,7 +65,6 @@ export default function AnalyticsScreen() {
           width={chartWidth}
           height={chartHeight}
           details={details}
-          uid={uid}
           isRound={false}
           backHomeButtonFn={() => navigation.navigate("Home", {screen: "ScoreTab"})}
         />
